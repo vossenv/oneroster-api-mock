855d671b1dbbd0116933509bc896f27519d59eca
Danimae Janssen <janssenda@gmail.com>
2018-06-26 17:18:16 -0500

    Initial

---
+++ src/main/java/com/dm/onerosterapi/OnerosterApiApplication.java
@@ -0,0 +1,12 @@
+package com.dm.onerosterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnerosterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnerosterApiApplication.class, args);
+	}
+}
