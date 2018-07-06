60be5de54d9365f848f2fc1e8157eac8f35eef89
Danimae Janssen <janssenda@gmail.com>
2018-07-06 13:49:38 -0500

    Updated helper test, commit to change to branch

--- api/src/test/java/com/dm/onerosterapi/service/helper.java
+++ api/src/test/java/com/dm/onerosterapi/service/helper.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -28,8 +29,22 @@ public class helper {
 	@Test
 	public void helperTest(){
 
-	    User u = userService.getUserById(25);
-	    List<User> userList = userService.getAllUsers();
+	    User u = new User();
+	    u.setSchoolId("1");
+
+	    helperService.idFieldSwap(u);
+	    assertEquals(u.getSchoolId(),"f9a75f84-130b-419e-bbe6-463585e930e9");
+
+	    List<User> userList = new ArrayList<>();
+	    userList.add(new User());
+	    userList.add(new User());
+
+	    userList.get(0).setSchoolId("1");
+	    userList.get(1).setSchoolId("2");
+
+	    helperService.idFieldSwap(userList);
+	    assertEquals(userList.get(0).getSchoolId(), "f9a75f84-130b-419e-bbe6-463585e930e9");
+        assertEquals(userList.get(1).getSchoolId(), "f5897384-9488-466f-b049-1992f7a53f15");
 
 
  	}
