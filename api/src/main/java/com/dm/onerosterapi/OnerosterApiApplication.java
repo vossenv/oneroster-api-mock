f5c751d0cade8227e6cae9088daafa2d1c18e9dc
Don R Walsh <donrwalsh@gmail.com>
2018-07-16 16:54:33 -0500

    Documentation for Classes Controller Endpoints
    Standard approach. Uses value sourced from Classlink. Selected to not use the default response times, electing to only use 200 currently (once Oauth is included this will need to be re-addressed)
    
    No effort expended on including example data, so Model is shown off twice.

--- api/src/main/java/com/dm/onerosterapi/OnerosterApiApplication.java
+++ api/src/main/java/com/dm/onerosterapi/OnerosterApiApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -18,11 +22,21 @@ public class OnerosterApiApplication {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                    .paths(regex("/classes.*"))
                 .paths(PathSelectors.any())
                 .build();
     };
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("OneRoster Mock API")
+                .description("Simple API based on the OneRoster specification")
+                .version("1.0")
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OnerosterApiApplication.class, args);
