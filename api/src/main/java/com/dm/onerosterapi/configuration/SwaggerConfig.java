01cd7de56ddbe0c74297a8ec5387c07ca3b9bb1f
Don R Walsh <donrwalsh@gmail.com>
2018-07-17 13:28:34 -0500

    Correcting Merge, Another bump to read time
    When I merged, I did not accurately retain the selection of all Controllers. This commit fixes that.
    
    I bumped up the read time from 10s to 15s after seeing a failure. I profiled the call with Chrome dev tools and see that it's loading at just over 10s, so 15s should be the sweet spot for this.

--- api/src/main/java/com/dm/onerosterapi/configuration/SwaggerConfig.java
+++ api/src/main/java/com/dm/onerosterapi/configuration/SwaggerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -23,7 +24,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .apiInfo(apiInfo())
                 .useDefaultResponseMessages(false)
                 .select()
-                .paths(regex("/classes.*"))
+                .apis(RequestHandlerSelectors.basePackage("com.dm.onerosterapi.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
