454111411c4e1360ad3459fc681da6d6c3e3b741
Danimae Janssen <janssenda@gmail.com>
2018-07-30 10:56:40 -0500

    Add version.properties

--- api/src/main/java/com/dm/onerosterapi/OnerosterApiApplication.java
+++ api/src/main/java/com/dm/onerosterapi/OnerosterApiApplication.java
@@ -1,7 +1,7 @@
 package com.dm.onerosterapi;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -10,8 +10,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableSpringDataWebSupport
 public class OnerosterApiApplication {
 
+//    public static void main(String[] args) {
+//        SpringApplication.run(OnerosterApiApplication.class, args);}
+
     public static void main(String[] args) {
-        SpringApplication.run(OnerosterApiApplication.class, args);
+        new SpringApplicationBuilder(OnerosterApiApplication.class)
+                .properties("spring.config.name:application,version")
+                .build()
+                .run(args);
+
     }
 
 }
--- api/src/main/java/com/dm/onerosterapi/controller/ViewController.java
+++ api/src/main/java/com/dm/onerosterapi/controller/ViewController.java
@@ -2,6 +2,7 @@ package com.dm.onerosterapi.controller;
 
 import com.dm.onerosterapi.apiconfig.AuthorizationServerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,15 @@ import java.util.Map;
 @Controller
 public class ViewController {
 
+    @Value("${version}")
+    private String version;
+
+    @Value("${build_date}")
+    private String date;
+
+    @Value("${commit_message}")
+    private String message;
+
     @Autowired
     AuthorizationServerConfig authConfig;
 
@@ -32,7 +42,10 @@ public class ViewController {
 
         Map<String, String> info = new LinkedHashMap<>();
 
-        info.put("requested resource",request.getRequestURI());
+        info.put("version",this.version);
+        info.put("build_date",this.date);
+        info.put("commit_message",this.message);
+
         info.put("full URL", request.getRequestURL().toString());
         info.putAll(authConfig.getToken());
 
