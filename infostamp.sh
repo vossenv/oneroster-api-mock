e446139dbe09c3ed24af97e35d0801e98014debc
Danimae Janssen <janssenda@gmail.com>
2018-07-31 11:40:14 -0500

    Test jenkins file to run stamp script
    This is a body

--- infostamp.sh
+++ infostamp.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
+path=$1
+if [[ $(echo $path | wc -w) -lt 1 ]]; then
+	path="api/src/main/resources/version.dat"
+fi
+
 function trim(){
 	echo $1 | awk '{gsub(/^ +| +$/,"")} {print $0}'
 }
 
-name=$(trim $(git log -1 --pretty=format:'%ce'))
-hash=$(trim $(git log -1 --pretty=format:'%H'))
-subject=$(trim $(git log -1 --pretty=format:'%s'))
+gitcmd="git log -1 --pretty=format:"
+
+name=$(trim "$(${gitcmd}'%ce')")
+hash=$(trim "$(${gitcmd}'%H')")
+subject=$(trim "$(${gitcmd}'%s')")
+body=$(trim "$(${gitcmd}'%b')")
 date=$(date +%m-%d-%Y' '%H:%M:%S)
 
-echo $name $date
-echo $(git log -1 --pretty=format:'%H' | tr -d '[:space:]') > test.txt
-echo $(trim $(git log -1 --pretty=format:'%ce')) >> test.txt
-var=$(git log -1 --pretty=format:'%s' | tr -d '[:space:]')
-var=$(date +%m-%d-%Y' '%H:%M:%S | tr -d '[:space:]')
+cat > $path << EOF
+committer_email = $name
+commit_hash = $hash
+build_timestamp = $date
+commit_subject = $subject
+commit_body = $body
+EOF
+
