30ac0ddf42aa3c968a99cf69969e716a68747d47
Danimae Janssen <janssenda@gmail.com>
2018-08-08 09:17:49 -0500

    Test crossorigin on view controller

--- api/src/main/java/com/dm/onerosterapi/controller/ViewController.java
+++ api/src/main/java/com/dm/onerosterapi/controller/ViewController.java
@@ -4,6 +4,7 @@ import com.dm.onerosterapi.apiconfig.AuthorizationServerConfig;
 import com.dm.onerosterapi.utility.PropertyGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +19,7 @@ import java.util.Map;
 
 @ApiIgnore
 @Controller
+@CrossOrigin
 @SuppressWarnings({"OptionalUsedAsFieldOrParameterType", "DefaultAnnotationParam"})
 public class ViewController {
 
