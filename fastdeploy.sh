fbd85b0a65ad47e24b7c9554129ba3b5087e57c0
Danimae Janssen <janssenda@gmail.com>
2018-11-08 16:36:01 -0600

    small change

--- fastdeploy.sh
+++ fastdeploy.sh
@@ -1,3 +1,4 @@
 ./gradlew clean assemble
 scp build/libs/oneroster-api-1.0.jar deployment@thenewcarag.com:/usr/springboot/oneroster
+scp orcert.pfx deployment@thenewcarag.com:/usr/springboot/oneroster
 ssh deployment@thenewcarag.com sudo service oneroster-api restart
